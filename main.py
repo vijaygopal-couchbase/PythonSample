ae8d027120ef6e967f8a4dee8e9846611f1d2790
vijaygopal-couchbase <62717469+vijaygopal-couchbase@users.noreply.github.com>
2024-03-22 12:39:31 -0500

    Initial commit

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import src.MovieSearch as ms
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +14,12 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+    sample_data = ms.load_sample_data()
+    bucket = ms.connect_to_capella()
+
+    ms.insert_into_capella(sample_data, bucket)
+    ms.search_movie(bucket)
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
